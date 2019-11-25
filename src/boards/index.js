0695d5b614d55e7560ec93a953ac99bf43480442
Jón Bjarni Ólafsson <jon.bjarni.olafsson@advania.is>
2019-11-25 15:30:55 +0000

    Styles for the board screen

--- src/boards/index.js
+++ src/boards/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, ScrollView, View, TouchableOpacity, StyleSheet } from 'react-native';
 import * as data from '../../db/data.json';
 
 class Boards extends Component {
@@ -24,14 +24,70 @@ class Boards extends Component {
 
     render() {
         const { boards } = this.state;
-        const boardlist = boards.map((x) => <Text key={x.id}>{x.name}</Text>);
+        const boardlist = boards.map((x) => {
+            return (
+                <View style={styles.boardListItem}>
+                    <Text style={styles.boardListItemText} key={x.id}>{x.name}</Text>
+                </View>
+            )
+        });
+
         return (
-            <View>
-                <Text>List of boards:</Text>
-                {boardlist}
-                <Button onPress={this.addToData} title="add data" />
+            <View style={{flex: 1, width: '100%'}}>
+                <View style={styles.container}>
+                    <Text style={styles.heading}>List of boards:</Text>
+                    <ScrollView>
+                        {boardlist}
+                    </ScrollView>
+
+                </View>
+                <TouchableOpacity style={styles.btn} onPress={this.addToData}>
+                    <Text style={styles.btnText}>Add item!</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#00A6FB',
+        width: '100%',
+        padding: 30,
+        paddingLeft: 15,
+        paddingRight: 15,
+    },
+    btn: { 
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        maxHeight: 50,
+        width: '100%',
+        backgroundColor: '#4CB944',
+    },
+    btnText:{
+        fontSize: 16,
+        fontWeight: 'normal',
+        color: '#fff',
+    },
+    heading: {
+        fontSize: 24,
+        color: '#fff',
+        fontWeight: 'bold',
+        marginBottom: 15,
+    },
+    boardListItem: {
+        flex: 1,
+        padding: 15,
+        backgroundColor: '#fff',
+        marginBottom: 30,
+    },
+
+    boardListItemText: {
+        fontSize: 16,
+        color: '#303030',
+    }
+})
+
 export default Boards;
