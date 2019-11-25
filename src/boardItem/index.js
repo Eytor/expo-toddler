3ee27f55dab38cd90f4efe0a71205cd79cb96598
eytor <eythoras16@ru.is>
2019-11-25 16:28:49 +0000

    start of item details

---
+++ src/boardItem/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BoardItem extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return ();
+    }
+
+}
+
+export default BoardItem;
