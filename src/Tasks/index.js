f9f5466073d81511d50617e0d9dece9ee207d015
Jón Bjarni Ólafsson <jon.bjarni.olafsson@advania.is>
2019-11-26 15:10:38 +0000

    Styles for task list items

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import * as data from '../../db/data.json';
 
@@ -19,24 +19,56 @@ class Tasks extends Component {
     }
 
     componentWillUnmount() {
-        data.tasks = this.state.tasks;
+        //data.tasks = this.state.tasks;
     }
 
     render() {
         const { tasks, id } = this.state;
         const list = tasks.map((element) => (
             <View key={element.id}>
-                <Text>{element.name}</Text>
-                <Text>{element.description}</Text>
-                <Text>{element.isFinished ? 'Finished' : 'Not Finished'}</Text>
+                <Text style={styles.heading}>{element.name}</Text>
+                <View style={[styles.contentWrapper, element.isFinished ? { borderColor: '#97CC04' } : { borderColor: '#D62828' }]}>
+                    <Text style={styles.text}>{element.description}</Text>
+                </View>
             </View>
         ));
-        return <View>{list}</View>;
+        return (
+            <View style={styles.container}>
+                {list}
+            </View>
+        );
     }
 }
 
 Tasks.propTypes = {
     navigation: PropTypes.object.isRequired,
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFA400',
+        width: '100%',
+        padding: 30,
+        paddingLeft: 15,
+        paddingRight: 15,
+    },
+    contentWrapper: {
+        padding: 15,
+        backgroundColor: '#fff',
+        marginBottom: 15,
+        borderLeftWidth: 5,
+    },
+    heading: {
+        fontSize: 24,
+        color: '#fff',
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    text:{
+        fontSize: 16,
+        color: '#303030',
+    }
+});
 
 export default Tasks;
