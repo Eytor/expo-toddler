37357d1d0e2d7b73dfa6a22370c407f65f727543
eytor <eythoras16@ru.is>
2019-11-26 13:30:07 +0000

    fixed all eslint errors

--- src/boardItem/index.js
+++ src/boardItem/index.js
@@ -1,37 +1,42 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
 import * as data from '../../db/data.json';
 
 class BoardItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lists: []
-        }
+            lists: [],
+        };
     }
 
     componentWillMount() {
         const { id } = this.props.navigation.state.params;
-        var list = data.lists.filter((element) => element.boardId === id);
-        this.setState({lists: list});
+        const list = data.lists.filter((element) => element.boardId === id);
+        this.setState({ lists: list });
     }
 
     render() {
-        var list = this.state.lists.map((element) => {
-            return (
-                <View key={element.id}>
+        const list = this.state.lists.map((element) => (
+            <View key={element.id}>
                 <Text>{element.name}</Text>
-                <View style={{backgroundColor: element.color, width: 15, height: 10}}></View>
+                <View
+                    style={{
+                        backgroundColor: element.color,
+                        width: 15,
+                        height: 10,
+                    }}
+                />
                 <Text>{element.boardId}</Text>
-                </View>
-            );
-        })
-        return (
-            <View>
-                {list}
             </View>
-        );
+        ));
+        return <View>{list}</View>;
     }
 }
+
+BoardItem.propTypes = {
+    navigation: PropTypes.object.isRequired,
+};
 
 export default BoardItem;
