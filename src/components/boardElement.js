4c8b431660f316ec75bf1fe85737b22387f40cc5
Jón Bjarni Ólafsson <jon.bjarni.olafsson@advania.is>
2019-11-26 13:16:15 +0000

    Adding more styles to the board/tickets.

--- src/components/boardElement.js
+++ src/components/boardElement.js
@@ -13,21 +13,31 @@ class BoardElement extends Component {
         const {id, name, description, thumbnailPhoto} = this.props;
         return (
             <View style={styles.boardListItem}>
-                <Image
-                    style={{ width: 50, height: 50 }}
-                    source={{ uri: thumbnailPhoto }}
-                />
-                <Text style={styles.boardListItemText}>{name}</Text>
-                <Text>{description}</Text>
+                <Text style={styles.heading}>{name}</Text>
+                <View style={styles.contentWrapper}>
+                    <View style={styles.imageWrapper}>
+                        <Image
+                            style={styles.image}
+                            source={{ uri: thumbnailPhoto }}
+                            resizeMode="contain"
+                        />
+                    </View>
+                    <View style={styles.desc}>
+                        <Text>{description}</Text>
+                    </View>
+                </View>
                 <TouchableOpacity
                     onPress={() => this.props.removeItem(id)}
+                    style={styles.deleteWrapper}
                 >
-                    <Text>delete</Text>
+                    <View style={styles.arrowTop}></View>
+                    <View style={styles.arrowBot}></View>
                 </TouchableOpacity>
                 <TouchableOpacity
                     onPress={() => this.props.openEdit(id, name, description, thumbnailPhoto)}
+                    style={styles.edit}
                 >
-                    <Text>Edit</Text>
+                    <Text style={styles.editText}>Edit</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -35,78 +45,75 @@ class BoardElement extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFA400',
-        width: '100%',
-        padding: 30,
-        paddingLeft: 15,
-        paddingRight: 15,
-    },
-    btn: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        maxHeight: 50,
-        width: '100%',
-        backgroundColor: '#4CB944',
-    },
-    btnText: {
-        fontSize: 16,
-        fontWeight: 'normal',
-        color: '#fff',
-    },
     heading: {
         fontSize: 24,
-        color: '#fff',
+        color: '#303030',
         fontWeight: 'bold',
         marginBottom: 15,
     },
     boardListItem: {
         flex: 1,
-        padding: 15,
+        padding: 10,
+        paddingLeft: 15,
+        paddingRight: 15,
         backgroundColor: '#fff',
         marginBottom: 30,
+        position: 'relative',
     },
-
+    contentWrapper: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    desc:{
+        flex: 1,
+        paddingLeft: 15,
+    },
+    imageWrapper:{
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        width: undefined,
+        height: undefined,
+        minHeight: 100,
+    },
     boardListItemText: {
         fontSize: 16,
         color: '#303030',
     },
-
-    modalWrapper: {
-        flex: 1,
-        padding: 30,
-        paddingLeft: 15,
-        paddingRight: 15,
-        backgroundColor: '#FFA400',
+    deleteWrapper: {
+        position: 'absolute',
+        right: 5,
+        top: 15,
+        width: 15,
+        height: 15,
     },
-
-    btnCloseModal: {
-        textTransform: 'uppercase',
-        fontSize: 25,
-        fontWeight: 'bold',
+    arrowTop: {
+        position: "absolute",
+        width: 2,
+        height: '100%',
+        top: 0,
+        left: 0,
+        transform: [{ rotate: '45deg' }],
+        backgroundColor: '#303030',
+    },
+    arrowBot: {
+        position: "absolute",
+        width: 2,
+        height: '100%',
+        bottom: 0,
+        left: 0,
+        transform: [{ rotate: '-45deg' }],
+        backgroundColor: '#303030',
+    },
+    editWrapper:{
+        marginTop: 10,
+    },
+    editText:{
         textAlign: 'right',
-    },
-    formGroup: {
-        marginBottom: 15,
-    },
 
-    modalLabel: {
-        fontSize: 16,
-        color: '#fff',
-        paddingLeft: 10,
-    },
+    }
 
-    modalInput: {
-        borderWidth: 0,
-        borderColor: '#303030',
-        borderRadius: 25,
-        backgroundColor: '#fff',
-        padding: 8,
-        paddingLeft: 15,
-        marginTop: 5,
-    },
 });
 
 export default BoardElement;
