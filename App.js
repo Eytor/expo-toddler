c83562b966a492650edbb321f168e14c5367b758
eytor <eythoras16@ru.is>
2019-11-26 10:38:39 +0000

    added stacknavigator

--- App.js
+++ App.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
 import AppNavigator from './src/navigation/navigator';
-import Boards from './src/boards/index';
 
 const AppContainer = createAppContainer(AppNavigator);
 
-export default class App extends React.Component {
-    render() {
-      return <AppContainer />;
-    }
-  }
+export default function App() {
+    return <AppContainer />;
+}
 
-// export default function App() {
-//     return (
-//         <View style={styles.container}>
-//             <Boards />
-//         </View>
-//     );
-// }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
